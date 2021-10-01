b815d64894f73d2836937c7e1e9b6cfe9a1e3243
Piotrek <99058308@mydavinci.nl>
2021-10-01 09:22:23 +0200

    Git

---
+++ person.py
@@ -0,0 +1,16 @@
+Naam = input("Wat is je naam?")
+print(Naam)
+
+Straat = input("Wat is je straat?")
+print(Straat) 
+
+Postcode = input("Wat is je postcode?")
+print(Postcode)
+
+Woonplaats = input("Wat is je woonplaats?")
+print(Woonplaats)
+
+print('Naam =' + Naam)
+print('Straat =' + Straat)
+print('Postcode =' + Postcode)
+print('Woonplaats =' + Woonplaats)
